a0601669e2e93e717207d759cdaf117743d3c715
Aidan Scannell <scannell.aidan@gmail.com>
2020-07-24 15:47:55 +0100

    Fixed mapping for multi gating network

--- mogpe/models/gating_network.py
+++ mogpe/models/gating_network.py
@@ -105,13 +105,15 @@ class SVGPGatingNetworkMulti(SVGPGatingNetworkBase):
 
         if likelihood is None:
             self.likelihood = Softmax(num_classes=self.num_experts)
+            # self.likelihood = Softmax(num_classes=1)
         else:
             self.likelihood = likelihood
 
     def predict_mixing_probs(self,
                              Xnew: InputData,
                              num_inducing_samples: int = None):
-        from mogpe.models.quadrature import ndiag_mc
+        from gpflow.quadrature import ndiag_mc
+        # from mogpe.models.quadrature import ndiag_mc
 
         mixing_probs = []
         Fmu, Fvar = [], []
@@ -122,27 +124,62 @@ class SVGPGatingNetworkMulti(SVGPGatingNetworkBase):
             Fvar.append(f_var)
         # Fmu = tf.stack(Fmu)
         # Fvar = tf.stack(Fvar)
-        Fmu = tf.concat(Fmu, -1)
-        Fvar = tf.concat(Fvar, -1)
-
-        def single_predict_mean(args):
-            Fmu, Fvar = args
-            integrand2 = lambda *X: self.likelihood.conditional_variance(
-                *X) + tf.square(self.likelihood.conditional_mean(*X))
-            epsilon = None
-            E_y, E_y2 = ndiag_mc(
-                [self.likelihood.conditional_mean, integrand2],
-                S=self.likelihood.num_monte_carlo_points,
-                Fmu=Fmu,
-                Fvar=Fvar,
-                epsilon=epsilon)
-            return E_y
-
+        Fmu = tf.stack(Fmu, -1)
+        Fvar = tf.stack(Fvar, -1)
+        # Fmu = tf.concat(Fmu, -1)
+        # Fvar = tf.concat(Fvar, -1)
         if num_inducing_samples is None:
-            mixing_probs = self.likelihood.predict_mean_and_var(Fmu, Fvar)[0]
+            Fmu = tf.transpose(Fmu, [1, 0, 2])
+            Fvar = tf.transpose(Fvar, [1, 0, 2])
         else:
-            mixing_probs = tf.map_fn(single_predict_mean, (Fmu, Fvar),
-                                     dtype=tf.float64)
+            Fmu = tf.transpose(Fmu, [2, 0, 1, 3])
+            Fvar = tf.transpose(Fvar, [2, 0, 1, 3])
+        print(Fmu.shape)
+        print(Fvar.shape)
+
+        # TODO map over output dimension
+
+        def single_output_predict_mean(args):
+            Fmu, Fvar = args
+            print('inside single output dim')
+            print(Fmu.shape)
+
+            def single_predict_mean(args):
+                Fmu, Fvar = args
+                print('inside single inducing sample')
+                print(Fmu.shape)
+                integrand2 = lambda *X: self.likelihood.conditional_variance(
+                    *X) + tf.square(self.likelihood.conditional_mean(*X))
+                epsilon = None
+                E_y, E_y2 = ndiag_mc(
+                    [self.likelihood.conditional_mean, integrand2],
+                    S=self.likelihood.num_monte_carlo_points,
+                    Fmu=Fmu,
+                    Fvar=Fvar,
+                    epsilon=epsilon)
+                return E_y
+
+            if num_inducing_samples is None:
+                mixing_probs = self.likelihood.predict_mean_and_var(Fmu,
+                                                                    Fvar)[0]
+            else:
+                # mixing_probs = tf.map_fn(single_predict_mean, (Fmu, Fvar),
+                #                          dtype=tf.float64)
+                mixing_probs = tf.vectorized_map(single_predict_mean,
+                                                 (Fmu, Fvar))
+            return mixing_probs
+
+        # mixing_probs = tf.map_fn(single_output_predict_mean, (Fmu, Fvar),
+        #                          dtype=tf.float64)
+        mixing_probs = tf.vectorized_map(single_output_predict_mean,
+                                         (Fmu, Fvar))
+        print(mixing_probs.shape)
+        if num_inducing_samples is None:
+            mixing_probs = tf.transpose(mixing_probs, [1, 0, 2])
+        else:
+            mixing_probs = tf.transpose(mixing_probs, [1, 2, 0, 3])
+        # mixing_probs = tf.transpose(mixing_probs, [1, 2, 0, 3])
+        print(mixing_probs.shape)
         return mixing_probs
 
 
@@ -194,8 +231,6 @@ class SVGPGatingNetworkBinary(SVGPGatingNetworkBase):
             prob_a_0 = tf.map_fn(single_predict_mean, (h_mu, h_var),
                                  dtype=tf.float64)
 
-        print('prob_a_0 in gating network mixing probs')
-        print(prob_a_0.shape)
         prob_a_1 = 1 - prob_a_0
         mixing_probs = tf.stack([prob_a_0, prob_a_1], -1)
         return mixing_probs
@@ -215,7 +250,6 @@ def init_fake_gating_network_binary(X, Y):
     noise_var = 0.1
     lengthscale = 1.
     mean_function = gpf.mean_functions.Zero()
-    likelihood = None
 
     q_mu = np.zeros(
         (num_inducing,
@@ -234,7 +268,6 @@ def init_fake_gating_network_binary(X, Y):
     kernel = gpf.kernels.SeparateIndependent(kern_list)
 
     gating_function = SVGPGatingFunction(kernel,
-                                         likelihood,
                                          inducing_variable,
                                          mean_function,
                                          q_mu=q_mu,
