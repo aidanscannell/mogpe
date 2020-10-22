6e03884099e23b5b7f98ff0d47b5b7ef06d9e68b
Aidan Scannell <scannell.aidan@gmail.com>
2020-10-22 14:39:20 +0100

    changed load quad to have 2 output dims and moved no data box

--- mogpe/data/utils.py
+++ mogpe/data/utils.py
@@ -65,41 +65,52 @@ def load_mcycle_dataset(filename='../../../data/external/mcycle.csv'):
 
 def load_quadcopter_dataset(
         filename='../../../data/processed/quadcopter_turbulence.npz',
+        # filename='../../../data/processed/quadcopter_turbulence_single_direction.npz',
+        # filename='../../../data/processed/quadcopter_turbulence_single_direction_with_reversed.npz',
         standardise=False):
     data = np.load(filename)
     X = data['x']
-    Y = data['y'][:, 0:1]
+    # Y = data['y'][:, 0:1]
     # Y = data['y'][:, 0:3]
     # X = data['x'][:, 0:1]
-    # Y = data['y'][:, 0:2]
-
-    X = tf.convert_to_tensor(X, dtype=default_float())
-    Y = tf.convert_to_tensor(Y, dtype=default_float())
+    Y = data['y'][:, 0:2]
     print("Input data shape: ", X.shape)
     print("Output data shape: ", Y.shape)
 
     # remove some data points
-    # if trim:
-    # def trim_dataset(X, Y, x1_low=-3., x2_low=-3., x1_high=0., x2_high=-1.):
-    #     mask_0 = X[:, 0] < x1_low
-    #     mask_1 = X[:, 1] < x2_low
-    #     mask_2 = X[:, 0] > x1_high
-    #     mask_3 = X[:, 1] > x2_high
-    #     mask = mask_0 | mask_1 | mask_2 | mask_3
-    #     X_partial = X[mask, :]
-    #     Y_partial = Y[mask, :]
-    #     x1 = [x1_low, x1_low, x1_high, x1_high, x1_low]
-    #     x2 = [x2_low, x2_high, x2_high, x2_low, x2_low]
-    #     X_missing = [x1, x2]
+    def trim_dataset(X, Y, x1_low=-3., x2_low=-3., x1_high=0., x2_high=-1.):
+        mask_0 = X[:, 0] < x1_low
+        mask_1 = X[:, 1] < x2_low
+        mask_2 = X[:, 0] > x1_high
+        mask_3 = X[:, 1] > x2_high
+        mask = mask_0 | mask_1 | mask_2 | mask_3
+        X_partial = X[mask, :]
+        Y_partial = Y[mask, :]
+        x1 = [x1_low, x1_low, x1_high, x1_high, x1_low]
+        x2 = [x2_low, x2_high, x2_high, x2_low, x2_low]
+        X_missing = [x1, x2]
 
-    #     print("New data shape:", Y_partial.shape)
-    #     return X_partial, Y_partial
-    # X_subset, Y_subset = trim_dataset(X,
-    #                                   Y,
-    #                                   x1_low=-3.,
-    #                                   x2_low=-3.,
-    #                                   x1_high=0.,
-    #                                   x2_high=-1.)
+        print("New data shape:", Y_partial.shape)
+        return X_partial, Y_partial
+
+    # X, Y = trim_dataset(X, Y, x1_low=-3., x2_low=-3., x1_high=0., x2_high=-1.)
+    # X, Y = trim_dataset(X, Y, x1_low=-1., x2_low=-0.5, x1_high=0., x2_high=1.5)
+    # X, Y = trim_dataset(X, Y, x1_low=-1., x2_low=-0.5, x1_high=1., x2_high=1.5)
+    # X, Y = trim_dataset(X, Y, x1_low=-0.5, x2_low=-3., x1_high=1., x2_high=1.)
+    # X, Y = trim_dataset(X, Y, x1_low=-1., x2_low=-3., x1_high=2., x2_high=-0.5)
+    # X, Y = trim_dataset(X, Y, x1_low=-1., x2_low=-3., x1_high=1., x2_high=0.)
+    X, Y = trim_dataset(X,
+                        Y,
+                        x1_low=-3.,
+                        x2_low=-0.5,
+                        x1_high=-0.5,
+                        x2_high=1.7)
+
+    X = tf.convert_to_tensor(X, dtype=default_float())
+    Y = tf.convert_to_tensor(Y, dtype=default_float())
+    import matplotlib.pyplot as plt
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.show()
 
     # standardise input
     if standardise:
