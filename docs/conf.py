8e4fee4e584c8e717da180772b9c25f562008c16
Aidan Scannell <scannell.aidan@gmail.com>
2021-01-20 17:38:03 +0000

    Removed tensorflow and tfp from autodoc to prevent failing.
    Also added check to see if local or on read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,27 @@
 # serve to show the default.
 import os
 import sys
-import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 # sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('~/Developer/python-projects/mogpe/mogpe'))
+# sys.path.insert(0, os.path.abspath('~/Developer/python-projects/mogpe/mogpe'))
+# sys.path.insert(0, os.path.abspath('../'))
+# sys.path.insert(0, os.path.abspath('../mogpe'))
+# sys.path.insert(0, os.path.abspath('~/Developer/python-projects/mogpe'))
+
+autodoc_mock_imports = ["tensorflow", "tensorflow_probability"]
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration -----------------------------------------------------
 
