2113dca6cb73731a7aa94e415137b9b4e4a75770
Aidan Scannell <scannell.aidan@gmail.com>
2021-01-21 14:34:47 +0000

    Added dephell commands to readme and updated setup.py/requirements.txt

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
     author='Aidan Scannell',
     author_email='scannell.aidan@gmail.com',
     license='Apache-2.0',
-    packages=['mogpe', 'mogpe.experts', 'mogpe.gating_networks', 'mogpe.gps', 'mogpe.helpers', 'mogpe.mixture_of_experts', 'mogpe.models_old', 'mogpe.training'],
+    packages=['mogpe', 'mogpe.experts', 'mogpe.gating_networks', 'mogpe.gps', 'mogpe.helpers', 'mogpe.mixture_of_experts', 'mogpe.training'],
     package_dir={"": "."},
-    package_data={"mogpe": ["*.org"]},
-    install_requires=['bunch==1.*,>=1.0.1', 'gpflow==2.*,>=2.1.3', 'jupyter==1.*,>=1.0.0', 'matplotlib==3.*,>=3.3.3', 'numpy==1.*,>=1.19.4', 'palettable==3.*,>=3.3.0', 'pandas==1.*,>=1.1.4', 'tensorflow==2.2.0', 'tensorflow-probability==0.*,>=0.10.0'],
-    extras_require={"dev": ["black==20.*,>=20.8.0.b1", "isort==5.*,>=5.6.4", "nbsphinx==0.*,>=0.8.0", "pyflakes==2.*,>=2.2.0", "recommonmark==0.*,>=0.6.0", "sphinx==3.*,>=3.3.1", "sphinx-autodoc-typehints==1.*,>=1.11.1", "sphinx-rtd-theme==0.*,>=0.5.0"]},
+    package_data={"mogpe": ["*.org"], "mogpe.mixture_of_experts": ["*.py#"]},
+    install_requires=['bunch==1.*,>=1.0.1', 'gpflow==2.*,>=2.1.3', 'jupyter==1.*,>=1.0.0', 'matplotlib==3.*,>=3.3.3', 'numpy==1.*,>=1.19.4', 'palettable==3.*,>=3.3.0', 'pandas==1.*,>=1.1.4', 'tensorflow==2.*,>=2.4.0', 'tensorflow-probability==0.*,>=0.12.1'],
+    extras_require={"dev": ["black==20.*,>=20.8.0.b1", "isort==5.*,>=5.6.4", "nbsphinx==0.*,>=0.8.0", "nbsphinx-link==1.*,>=1.1.0", "pyflakes==2.*,>=2.2.0", "recommonmark==0.*,>=0.6.0", "sphinx==3.*,>=3.4.0", "sphinx-autodoc-typehints==1.*,>=1.11.1", "sphinx-rtd-theme==0.*,>=0.5.0"]},
 )
