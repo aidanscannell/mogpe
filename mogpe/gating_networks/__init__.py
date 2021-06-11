b9170bc4b0fe071a96e71861789471f76d86598c
Aidan Scannell <scannell.aidan@gmail.com>
2021-06-11 11:17:15 +0100

    Changed SVGPGatingNetwork to be an SVGP classificqation model
    likelihood can either be Bernoulli or Softmax

--- mogpe/gating_networks/__init__.py
+++ mogpe/gating_networks/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 from mogpe.gating_networks.base import GatingNetworkBase
-from mogpe.gating_networks.svgp import SVGPGatingNetworkBinary, SVGPGatingNetworkMulti, SVGPGatingFunction, SVGPGatingNetworkBase
+from mogpe.gating_networks.svgp import SVGPGatingNetwork
