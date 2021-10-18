b19eed5729de492ed10dcb5dbff4d5a70432e6ea
Aidan Scannell <scannell.aidan@gmail.com>
2021-10-18 09:27:27 +0100

    implemented test/train split for mcycle

--- examples/mcycle/data/load_data.py
+++ examples/mcycle/data/load_data.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python3
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from gpflow import default_float
+from sklearn.model_selection import KFold, train_test_split
 
 
 def load_mcycle_dataset(
-    filename: str = "./mcycle.csv", plot: bool = False, standardise: bool = True
+    filename: str = "./mcycle.csv",
+    plot: bool = False,
+    standardise: bool = True,
+    test_split_size=0.0,
 ):
     data = np.loadtxt(filename, delimiter=",", skiprows=1, usecols=(1, 2))
     X = data[:, 0].reshape(-1, 1)
     Y = data[:, 1].reshape(-1, 1)
 
-    X = tf.convert_to_tensor(X, dtype=default_float())
-    Y = tf.convert_to_tensor(Y, dtype=default_float())
     print("Input data shape: ", X.shape)
     print("Output data shape: ", Y.shape)
 
     # standardise input
     if standardise:
-        mean_x, var_x = tf.nn.moments(X, axes=[0])
-        mean_y, var_y = tf.nn.moments(Y, axes=[0])
-        X = (X - mean_x) / tf.sqrt(var_x)
-        Y = (Y - mean_y) / tf.sqrt(var_y)
+        X = (X - X.mean()) / X.std()
+        Y = (Y - Y.mean()) / Y.std()
+
+    if test_split_size > 0:
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=test_split_size, random_state=42
+        )
+        X_test = tf.convert_to_tensor(X_test, dtype=default_float())
+        Y_test = tf.convert_to_tensor(Y_test, dtype=default_float())
+        test_data = (X_test, Y_test)
+    else:
+        X_train, Y_train = X, Y
+
+    X_train = tf.convert_to_tensor(X_train, dtype=default_float())
+    Y_train = tf.convert_to_tensor(Y_train, dtype=default_float())
+    train_data = (X_train, Y_train)
+
     if plot:
-        plt.scatter(X, Y)
+        plt.scatter(X_train, Y_train, label="Train", color="k")
+        if test_split_size > 0:
+            plt.scatter(X_test, Y_test, label="Test", color="r")
+        plt.legend()
         plt.show()
-    data = (X, Y)
-    return data
+
+    print("Train input data shape: ", X_train.shape)
+    print("Train output data shape: ", Y_train.shape)
+    if test_split_size > 0:
+        print("Test input data shape: ", X_test.shape)
+        print("Test output data shape: ", Y_test.shape)
+        return train_data, test_data
+    else:
+        return train_data, None
+
+
+# # X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+# # y = np.array([1, 2, 3, 4])
+# kf = KFold(n_splits=2)
+# kf.get_n_splits(X)
+# KFold(n_splits=2, random_state=None, shuffle=False)
+# for train_index, test_index in kf.split(X):
+#     print("TRAIN:", train_index, "TEST:", test_index)
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
--- examples/mcycle/train_mogpe_on_mcycle_from_config.py
+++ examples/mcycle/train_mogpe_on_mcycle_from_config.py
@@ -7,7 +7,11 @@ from mogpe.helpers.plotter import Plotter1D
 
 # from mogpe.training import create_mosvgpe_model_from_config, monitored_training_loop
 from mogpe.training import MixtureOfSVGPExperts_from_toml, monitored_training_loop
-from mogpe.training.utils import create_log_dir, create_tf_dataset, init_fast_tasks
+from mogpe.training.utils import (
+    create_log_dir,
+    create_tf_dataset,
+    init_fast_tasks_bounds,
+)
 from mcycle.data.load_data import load_mcycle_dataset
 
 tf.random.set_seed(42)
@@ -28,21 +32,42 @@ def train_mogpe_on_mcycle_given_config(
         plot = cfg.plot_dataset
     except AttributeError:
         plot = False
-    dataset = load_mcycle_dataset(cfg.data_file, plot=plot, standardise=standardise)
+    try:
+        test_split_size = cfg.test_split_size
+    except AttributeError:
+        test_split_size = False
+    # dataset = load_mcycle_dataset(
+    #     cfg.data_file,
+    #     plot=plot,
+    #     standardise=standardise,
+    #     test_split_size=test_split_size,
+    # )
+    train_dataset, test_dataset = load_mcycle_dataset(
+        cfg.data_file,
+        plot=plot,
+        standardise=standardise,
+        test_split_size=test_split_size,
+    )
 
     # Parse the toml config file to create MixtureOfSVGPExperts model
-    model = MixtureOfSVGPExperts_from_toml(config_file, dataset)
+    model = MixtureOfSVGPExperts_from_toml(config_file, train_dataset)
     gpf.utilities.print_summary(model)
-    gpf.set_trainable(model.gating_network.inducing_variable, False)
-    for expert in model.experts.experts_list:
-        gpf.set_trainable(expert.inducing_variable, False)
+    # gpf.set_trainable(model.gating_network.inducing_variable, False)
+    # for expert in model.experts.experts_list:
+    #     gpf.set_trainable(expert.inducing_variable, False)
     gpf.utilities.print_summary(model)
 
     # Create tf dataset that can be iterated and build training loss closure
-    train_dataset, num_batches_per_epoch = create_tf_dataset(
-        dataset, num_data=dataset[0].shape[0], batch_size=cfg.batch_size
+    train_dataset_tf, num_batches_per_epoch = create_tf_dataset(
+        train_dataset, num_data=train_dataset[0].shape[0], batch_size=cfg.batch_size
     )
-    training_loss = model.training_loss_closure(iter(train_dataset))
+    if test_dataset is not None:
+        test_dataset_tf, _ = create_tf_dataset(
+            test_dataset, num_data=test_dataset[0].shape[0], batch_size=cfg.batch_size
+        )
+    else:
+        test_dataset_tf = None
+    training_loss = model.training_loss_closure(iter(train_dataset_tf))
 
     # Create monitor tasks (plots/elbo/model params etc)
     log_dir = create_log_dir(
@@ -53,9 +78,17 @@ def train_mogpe_on_mcycle_given_config(
         bound=cfg.bound,
         num_inducing=cfg.experts[0]["inducing_points"]["num_inducing"],
     )
-    plotter = Plotter1D(model, X=dataset[0], Y=dataset[1])
+    plotter = Plotter1D(model, X=train_dataset[0], Y=train_dataset[1])
     slow_tasks = plotter.tf_monitor_task_group(log_dir, cfg.slow_tasks_period)
-    fast_tasks = init_fast_tasks(log_dir, model, training_loss, cfg.fast_tasks_period)
+    # fast_tasks = init_fast_tasks(log_dir, model, training_loss, cfg.fast_tasks_period)
+    fast_tasks = init_fast_tasks_bounds(
+        log_dir,
+        train_dataset_tf,
+        model,
+        test_dataset=test_dataset_tf,
+        training_loss=training_loss,
+        fast_tasks_period=cfg.fast_tasks_period,
+    )
 
     # Init checkpoint manager for saving model during training
     ckpt = tf.train.Checkpoint(model=model)
@@ -77,7 +110,11 @@ def train_mogpe_on_mcycle_given_config(
 
 if __name__ == "__main__":
     # Load config (with model and training params) from toml file
-    config_file = "./mcycle/configs/config_2_experts_subset.toml"  # path to config
-    # config_file = "./mcycle/configs/config_3_experts_subset.toml"  # path to config
+    # config_file = "./mcycle/configs/config_2_experts.toml"  # path to config
+    # config_file = "./mcycle/configs/config_3_experts.toml"  # path to config
+    # config_file = "./mcycle/configs/config_3_experts_test.toml"  # path to config
+    # config_file = "./mcycle/configs/config_2_experts_test.toml"  # path to config
+    # config_file = "./mcycle/configs/config_2_experts_full.toml"  # path to config
+    config_file = "./mcycle/configs/config_3_experts_full.toml"  # path to config
 
     train_mogpe_on_mcycle_given_config(config_file=config_file)
