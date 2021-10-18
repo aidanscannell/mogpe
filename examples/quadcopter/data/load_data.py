c08f05bb3feb96acb193bae416a20981b651f110
Aidan Scannell <scannell.aidan@gmail.com>
2021-10-18 09:35:49 +0100

    implemented test/train split for quadcopter

--- examples/quadcopter/data/load_data.py
+++ examples/quadcopter/data/load_data.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import matplotlib.pyplot as plt
 from typing import Optional
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from gpflow import default_float
+from sklearn.model_selection import KFold, train_test_split
 
 # Define input region (rectangle) to remove data from.
 # This is done to test the models ability to capture epistemic unc.
@@ -25,7 +27,7 @@ def load_quadcopter_dataset(
     num_outputs: Optional[int] = None,
     plot: Optional[bool] = False,
     standardise: Optional[bool] = True,
-    # standardise: Optional[bool] = False,
+    test_split_size=0.0,
 ):
     data = np.load(filename)
     if num_inputs is not None:
@@ -62,20 +64,67 @@ def load_quadcopter_dataset(
         X, Y = trim_dataset(X, Y, trim_coords)
         # X, Y, x1_low=x1_low, x2_low=x2_low, x1_high=x1_high, x2_high=x2_high
 
-    X = tf.convert_to_tensor(X, dtype=default_float())
-    Y = tf.convert_to_tensor(Y, dtype=default_float())
+    # X = tf.convert_to_tensor(X, dtype=default_float())
+    # Y = tf.convert_to_tensor(Y, dtype=default_float())
     print("Trimmed input data shape: ", X.shape)
     print("Trimmed output data shape: ", Y.shape)
 
     # standardise input
-    if standardise:
-        mean_x, var_x = tf.nn.moments(X, axes=[0])
-        mean_y, var_y = tf.nn.moments(Y, axes=[0])
-        X = (X - mean_x) / tf.sqrt(var_x)
-        Y = (Y - mean_y) / tf.sqrt(var_y)
+    # if standardise:
+    #     mean_x, var_x = tf.nn.moments(X, axes=[0])
+    #     mean_y, var_y = tf.nn.moments(Y, axes=[0])
+    #     X = (X - mean_x) / tf.sqrt(var_x)
+    #     Y = (Y - mean_y) / tf.sqrt(var_y)
+    # if standardise:
+    #     X = (X - X.mean()) / X.std()
+    #     Y = (Y - Y.mean()) / Y.std()
+
+    # if plot:
+    #     plt.quiver(X[:, 0], X[:, 1], Y[:, 0], Y[:, 1])
+    #     plt.show()
+    # data = (X, Y)
+    # return data
+
+    if test_split_size > 0:
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=test_split_size, random_state=42
+        )
+        X_test = tf.convert_to_tensor(X_test, dtype=default_float())
+        Y_test = tf.convert_to_tensor(Y_test, dtype=default_float())
+        test_data = (X_test, Y_test)
+    else:
+        X_train, Y_train = X, Y
+
+    X_train = tf.convert_to_tensor(X_train, dtype=default_float())
+    Y_train = tf.convert_to_tensor(Y_train, dtype=default_float())
+    train_data = (X_train, Y_train)
 
     if plot:
-        plt.quiver(X[:, 0], X[:, 1], Y[:, 0], Y[:, 1])
+        plt.quiver(
+            X_train[:, 0],
+            X_train[:, 1],
+            Y_train[:, 0],
+            Y_train[:, 1],
+            label="Train",
+            color="k",
+        )
+        if test_split_size > 0:
+            plt.quiver(
+                X_test[:, 0],
+                X_test[:, 1],
+                Y_test[:, 0],
+                Y_test[:, 1],
+                label="Test",
+                color="r",
+            )
+        plt.legend()
         plt.show()
-    data = (X, Y)
-    return data
+
+    print("Train input data shape: ", X_train.shape)
+    print("Train output data shape: ", Y_train.shape)
+    if test_split_size > 0:
+        print("Test input data shape: ", X_test.shape)
+        print("Test output data shape: ", Y_test.shape)
+        return train_data, test_data
+    else:
+        return train_data
--- examples/quadcopter/train_mogpe_on_quadcopter_given_config.py
+++ examples/quadcopter/train_mogpe_on_quadcopter_given_config.py
@@ -7,7 +7,11 @@ from mogpe.helpers.plotter import Plotter2D
 
 # from mogpe.training import create_mosvgpe_model_from_config, monitored_training_loop
 from mogpe.training import MixtureOfSVGPExperts_from_toml, monitored_training_loop
-from mogpe.training.utils import create_log_dir, create_tf_dataset, init_fast_tasks
+from mogpe.training.utils import (
+    create_log_dir,
+    create_tf_dataset,
+    init_fast_tasks_bounds,
+)
 from quadcopter.data.load_data import load_quadcopter_dataset
 
 tf.random.set_seed(42)
@@ -16,9 +20,7 @@ np.random.seed(42)
 
 def train_mogpe_on_quadcopter_given_config(
     config_file: str,
-    # config_file: str, data_file: str, log_dir: str = "./logs/quadcopter"
 ):
-    # config_file = "./quadcopter/configs/config_3_experts.toml"  # path to config
     cfg = config_from_toml(config_file, read_from_file=True)
 
     # Load quadcopter data set
@@ -34,31 +36,60 @@ def train_mogpe_on_quadcopter_given_config(
         plot = cfg.plot_dataset
     except AttributeError:
         plot = False
-    dataset = load_quadcopter_dataset(
+    try:
+        test_split_size = cfg.test_split_size
+    except AttributeError:
+        test_split_size = False
+    train_dataset, test_dataset = load_quadcopter_dataset(
         cfg.data_file,
         trim_coords=trim_coords,
         num_outputs=2,
         plot=plot,
         standardise=standardise,
+        test_split_size=test_split_size,
     )
 
     # Parse the toml config file to create MixtureOfSVGPExperts model
-    model = MixtureOfSVGPExperts_from_toml(config_file, dataset)
+    model = MixtureOfSVGPExperts_from_toml(config_file, train_dataset)
+    gpf.utilities.print_summary(model)
+    gpf.set_trainable(model.gating_network.inducing_variable, False)
+    for expert in model.experts.experts_list:
+        gpf.set_trainable(expert.inducing_variable, False)
     gpf.utilities.print_summary(model)
 
     # Create tf dataset that can be iterated and build training loss closure
-    train_dataset, num_batches_per_epoch = create_tf_dataset(
-        dataset, num_data=dataset[0].shape[0], batch_size=cfg.batch_size
+    # train_dataset, num_batches_per_epoch = create_tf_dataset(
+    #     dataset, num_data=dataset[0].shape[0], batch_size=cfg.batch_size
+    # )
+    # training_loss = model.training_loss_closure(iter(train_dataset))
+    train_dataset_tf, num_batches_per_epoch = create_tf_dataset(
+        train_dataset, num_data=train_dataset[0].shape[0], batch_size=cfg.batch_size
     )
-    training_loss = model.training_loss_closure(iter(train_dataset))
+    test_dataset_tf, _ = create_tf_dataset(
+        test_dataset, num_data=test_dataset[0].shape[0], batch_size=cfg.batch_size
+    )
+    training_loss = model.training_loss_closure(iter(train_dataset_tf))
 
     # Create monitor tasks (plots/elbo/model params etc)
     log_dir = create_log_dir(
-        cfg.log_dir, model.num_experts, cfg.batch_size, learning_rate=cfg.learning_rate
+        cfg.log_dir,
+        model.num_experts,
+        cfg.batch_size,
+        learning_rate=cfg.learning_rate,
+        bound=cfg.bound,
+        num_inducing=cfg.experts[0]["inducing_points"]["num_inducing"],
     )
-    plotter = Plotter2D(model, X=dataset[0], Y=dataset[1])
+    plotter = Plotter2D(model, X=train_dataset[0], Y=train_dataset[1])
     slow_tasks = plotter.tf_monitor_task_group(log_dir, cfg.slow_tasks_period)
-    fast_tasks = init_fast_tasks(log_dir, model, training_loss, cfg.fast_tasks_period)
+    # fast_tasks = init_fast_tasks(log_dir, model, training_loss, cfg.fast_tasks_period)
+    fast_tasks = init_fast_tasks_bounds(
+        log_dir,
+        train_dataset_tf,
+        model,
+        test_dataset=test_dataset_tf,
+        training_loss=training_loss,
+        fast_tasks_period=cfg.fast_tasks_period,
+    )
 
     # Init checkpoint manager for saving model during training
     ckpt = tf.train.Checkpoint(model=model)
@@ -81,6 +112,14 @@ def train_mogpe_on_quadcopter_given_config(
 if __name__ == "__main__":
     # Load config (with model and training params) from toml file
     config_file = "./quadcopter/configs/config_2_experts_subset.toml"  # path to config
-    config_file = "./quadcopter/configs/config_2_experts_full.toml"  # path to config
+    # config_file = "./quadcopter/configs/config_2_experts_full.toml"  # path to config
+    config_file = (
+        "./quadcopter/configs/config_2_experts_subset_10.toml"  # path to config
+    )
+    # config_file = (
+    #     # "./quadcopter/configs/config_2_experts_subset_10.toml"  # path to config
+    #     # "./quadcopter/configs/config_2_experts_subset_40.toml"  # path to config
+    #     "./quadcopter/configs/config_3_experts_subset_10.toml"  # path to config
+    # )
 
     train_mogpe_on_quadcopter_given_config(config_file=config_file)
