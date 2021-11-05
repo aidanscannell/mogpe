6029e22dde05f3667526323c4675c48c8da761e8
Aidan Scannell <scannell.aidan@gmail.com>
2021-11-05 09:28:19 +0000

    added ckpt for further_gating_bound which am using in thesis

--- examples/quadcopter/plotting/plot_model.py
+++ examples/quadcopter/plotting/plot_model.py
@@ -6,14 +6,20 @@ from mogpe.training import load_model_from_config_and_checkpoint
 from quadcopter.data.load_data import load_quadcopter_dataset
 
 if __name__ == "__main__":
-    # ckpt_dir = "./quadcopter/saved_ckpts/subset/2_experts/batch_size_64/learning_rate_0.01/09-29-094538"
-    ckpt_dir = "./quadcopter/saved_ckpts/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_bound/10-11-104617"
+    ckpt_dir = "./quadcopter/saved_ckpts/subset/2_experts/batch_size_64/learning_rate_0.01/09-29-094538"
+    config_file = "./quadcopter/configs/config_2_experts_subset.toml"  # path to config
+    save_dir = "./quadcopter/images/subset-yo"
+
+    # # ckpt_dir = "./quadcopter/saved_ckpts/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_bound/10-11-104617"
+    ckpt_dir = "./quadcopter/saved_ckpts/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_gating_bound/num_inducing_100/11-04-210440"
+    config_file = "./quadcopter/configs/config_2_experts_subset_10.toml"
+    save_dir = "./quadcopter/images/subset-10"
+
+    # ckpt_dir = "./logs/quadcopter/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_gating_bound/num_inducing_100/11-04-152145"
+    # config_file = "./quadcopter/configs/config_2_experts_subset_10_icra.toml"
+    # save_dir = "./quadcopter/images/subset-10-icra"
 
     # Load config (with model and training params) from toml file
-    # config_file = "./quadcopter/configs/config_2_experts_subset.toml"  # path to config
-    config_file = (
-        "./quadcopter/configs/config_2_experts_subset_10.toml"  # path to config
-    )
     cfg = config_from_toml(config_file, read_from_file=True)
 
     # Load quadcopter data set
@@ -21,7 +27,7 @@ if __name__ == "__main__":
         trim_coords = np.array(cfg.trim_coords)
     else:
         trim_coords = None
-    dataset = load_quadcopter_dataset(
+    dataset, _ = load_quadcopter_dataset(
         cfg.data_file,
         trim_coords=trim_coords,
         num_outputs=2,
@@ -33,4 +39,4 @@ if __name__ == "__main__":
     plotter = QuadcopterPlotter(model, X=dataset[0], Y=dataset[1])
     # plotter.plot_experts_f("./quadcopter/images/subset/test_new.pdf")
     # plotter.plot_model("./quadcopter/images/subset")
-    plotter.plot_model("./quadcopter/images/subset-10")
+    plotter.plot_model(save_dir)
