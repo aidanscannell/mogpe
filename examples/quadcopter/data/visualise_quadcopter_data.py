22f5648051501cad317335a831d76e6a2e8f2b0a
Aidan Scannell <scannell.aidan@gmail.com>
2021-11-05 09:36:13 +0000

    reformatted

--- examples/quadcopter/data/visualise_quadcopter_data.py
+++ examples/quadcopter/data/visualise_quadcopter_data.py
@@ -8,15 +8,20 @@ params = {
     # 'legend.fontsize': 20,
     # 'xtick.labelsize': 30,
     # 'ytick.labelsize': 30,
-    'text.usetex': True,
+    "text.usetex": True,
 }
 plt.rcParams.update(params)
 
-load_npz_filename = './quadcopter_data.npz'
+load_npz_filename = "./quadcopter_data.npz"
+load_npz_filename = "./quadcopter_data_step_20.npz"
+# load_npz_filename = "./quadcopter_data_step_40.npz"
+load_npz_filename = "./quadcopter_data_step_20_single_direction.npz"
+load_npz_filename = "./quadcopter_data_step_40_direction_up.npz"
+# load_npz_filename = "./quadcopter_data_step_40_direction_down.npz"
 
 data = np.load(load_npz_filename)
-X = data['x']
-Y = data['y']
+X = data["x"]
+Y = data["y"]
 
 fig = plt.figure()
 plt.quiver(
@@ -24,12 +29,13 @@ plt.quiver(
     X[:, 1],
     Y[:, 0],
     Y[:, 1],
-    angles='xy',
-    scale_units='xy',
+    angles="xy",
+    scale_units="xy",
     width=0.001,
     scale=1,
-    zorder=10)
-plt.xlabel('$x$')
-plt.ylabel('$y$')
+    zorder=10,
+)
+plt.xlabel("$x$")
+plt.ylabel("$y$")
 # plt.axis('off')
 plt.show()
