8d406a242e9472a7df1a2296d184eaf38d9af7ae
Aidan Scannell <scannell.aidan@gmail.com>
2021-12-08 15:06:14 +0000

    commit used for getting thesis results

--- examples/quadcopter/plotting/plot_model.py
+++ examples/quadcopter/plotting/plot_model.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 import numpy as np
+import tensorflow as tf
 from config import config_from_toml
 from mogpe.helpers.quadcopter_plotter import QuadcopterPlotter
 from mogpe.training import load_model_from_config_and_checkpoint
 from quadcopter.data.load_data import load_quadcopter_dataset
 
 if __name__ == "__main__":
-    ckpt_dir = "./quadcopter/saved_ckpts/subset/2_experts/batch_size_64/learning_rate_0.01/09-29-094538"
-    config_file = "./quadcopter/configs/config_2_experts_subset.toml"  # path to config
-    save_dir = "./quadcopter/images/subset-yo"
+    # ckpt_dir = "./quadcopter/saved_ckpts/subset/2_experts/batch_size_64/learning_rate_0.01/09-29-094538"
+    # config_file = "./quadcopter/configs/config_2_experts_subset.toml"  # path to config
+    # save_dir = "./quadcopter/images/subset-yo"
 
     # # ckpt_dir = "./quadcopter/saved_ckpts/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_bound/10-11-104617"
     ckpt_dir = "./quadcopter/saved_ckpts/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_gating_bound/num_inducing_100/11-04-210440"
-    config_file = "./quadcopter/configs/config_2_experts_subset_10.toml"
     save_dir = "./quadcopter/images/subset-10"
 
     # ckpt_dir = "./logs/quadcopter/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_gating_bound/num_inducing_100/11-04-152145"
     # config_file = "./quadcopter/configs/config_2_experts_subset_10_icra.toml"
     # save_dir = "./quadcopter/images/subset-10-icra"
 
+    ckpt_dir = "./logs/quadcopter/subset-10/2_experts/batch_size_64/learning_rate_0.01/further_gating_bound/num_inducing_100/11-05-104542"
+    save_dir = "./quadcopter/images/subset-10"
+
     # Load config (with model and training params) from toml file
+    try:
+        if config_file is None:
+            config_file = ckpt_dir + "/config.toml"
+    except:
+        config_file = ckpt_dir + "/config.toml"
     cfg = config_from_toml(config_file, read_from_file=True)
 
     # Load quadcopter data set
