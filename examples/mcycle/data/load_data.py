4e8aa99d23766eb957a244123cdae44e15957598
Aidan Scannell <scannell.aidan@gmail.com>
2021-12-08 15:01:25 +0000

    removed unused KFold import

--- examples/mcycle/data/load_data.py
+++ examples/mcycle/data/load_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from gpflow import default_float
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import train_test_split
 
 
 def load_mcycle_dataset(
